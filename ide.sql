0b0f889946ab23d66584a5e46d434aafabcbf5da
Vincentius Daryl Kurniawan <6181901023@student.unpar.ac.id>
2022-06-08 22:07:27 +0700

    Ubah ide.sql, tambah foreign key

--- ide.sql
+++ ide.sql
@@ -1,32 +1,28 @@
 
 
 -- Drop Table
-DROP TABLE Pengguna;
-DROP TABLE Customer;
-DROP TABLE BahanBaku;
-DROP TABLE Aksesoris;
-DROP TABLE ModelBaju;
-DROP TABLE Baju;
-DROP TABLE Transaksi;
-DROP TABLE Kurir;
+DROP TABLE IF EXISTS Pengguna;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS BahanBaku;
+DROP TABLE IF EXISTS Aksesoris;
+DROP TABLE IF EXISTS ModelBaju;
+DROP TABLE IF EXISTS Baju;
+DROP TABLE IF EXISTS Transaksi;
+DROP TABLE IF EXISTS Kurir;
 
 -- BUAT TABLE
 
 -- Buat Table Pengguna
 CREATE TABLE Pengguna (
-	idPengguna int,
+	idPengguna int AUTO_INCREMENT,
     Email varchar(30),
     Username varchar(20),
     Pass varchar(20),
-    namaPengguna varchar(20)
-);
-
-
--- Buat Table Customer
-CREATE TABLE Customer (
-	namaCustomer varchar(20), 
-    Alamat varchar(30),
-    noHP varchar(14)
+    namaPengguna varchar(20),
+    peran varchar(20),
+    alamat varchar(50) NOT NULL,
+    noHP varchar(15) NOT NULL,
+    PRIMARY KEY(idPengguna)
 );
 
 
@@ -36,23 +32,32 @@ CREATE TABLE BahanBaku (
     namaBahanBaku varchar(20), 
     hargaBeliBahan decimal NOT NULL,
     hargaJualBahan decimal NOT NULL
+    deskripsiBahanBaku varchar(50) NOT NULL,
+    gambarBahanBaku varchar(100) NOT NULL,
+    PRIMARY KEY(idBahanBaku),
 );
 
 
 -- Buat Table Aksesoris
 CREATE TABLE Aksesoris (
-	idAksesoris int,
+	idAksesoris int AUTO_INCREMENT,
     namaAksesoris varchar(20),
     hargaBeliAksesoris decimal NOT NULL,
     hargaJualAksesoris decimal NOT NULL
+    deskripsiAksesoris varchar(50) NOT NULL,
+    gambarAksesoris varchar(100) NOT NULL,
+    PRIMARY KEY(idAksesoris)
 );
 
 
 -- Buat Table Model Baju
 CREATE TABLE ModelBaju (
-	idModel int, 
+	idModel int AUTO_INCREMENT, 
     namaModel varchar(20),
-    hargaModel decimal NOT NULL
+    deskripsiModel varchar(50) NOT NULL,
+    hargaModel decimal NOT NULL,
+    gambarModel varchar(50) NOT NULL,
+    PRIMARY KEY(idModel)
 );
 
 
@@ -61,87 +66,62 @@ CREATE TABLE Baju (
 	idBaju int,
     Ukuran varchar(5),
     idBahanBaku int,
-    idAksesoris int,
     idModel int,
     hargaBaju decimal NOT NULL,
-    hargaJahit decimal NOT NULL
+    PRIMARY KEY(idBaju),
+    INDEX (idBahanBaku),
+    INDEX(idModel),
+    FOREIGN KEY (idBahanBaku)
+        REFERENCES BahanBaku(idBahanBaku),
+    FOREIGN KEY (idModel)
+        REFERENCES ModelBaju(idModel)
 );
 
 
 -- Buat Table Kurir
 CREATE TABLE Kurir (
-	idKurir int, 
-    namaKurir varchar(20)
+	idKurir int AUTO_INCREMENT,
+    jenisKurir int,
+    PRIMARY KEY(idKurir)
 );
 
 
 -- Buat Table Transaksi
 CREATE TABLE Transaksi (
-	idTransaksi int, 
+	idTransaksi int AUTO_INCREMENT,
     idPengguna int,
+    idAksesoris int NOT NULL,
+    idBaju int NOT NULL,
     idKurir int, 
     tglTransaksi date,
-    statPemesanan varchar(20),
-    statPembayaran varchar(5),
+    statPemesanan varchar(30),
+    statPembayaran varchar(30),
     buktiTransfer varchar(50)
+    hargaTotal int(20) NOT NULL,
+    PRIMARY KEY(idTransaksi),
+    INDEX(idPengguna),
+    INDEX(idAksesoris),
+    Index(idBaju),
+    INDEX(idKurir),
+    FOREIGN KEY (idPengguna)
+        REFERENCES Pengguna(idPengguna),
+    FOREIGN KEY (idBaju)
+        REFERENCES Baju(idBaju),
+    FOREIGN KEY (idKurir)
+        REFERENCES Kurir(idKurir),
+    FOREIGN KEY (idAksesoris),
+        REFERENCES Aksesoris(idAksesoris)
 );
 
 
 -- INSERT TABLE
 
 -- Insert Table Pengguna
-INSERT INTO Pengguna(idPengguna, Email, Username, Pass, namaPengguna) VALUES
-('1', 'admin@gmail.com', 'admin', 'admin', 'admin'),
-('2', 'pemilik@gmail.com', 'pemilik', 'pemilik', 'pemilik'),
-('3', 'joko@gmail.com', 'joko', 'joko1234', 'Joko');
-
-INSERT INTO Customer(namaCustomer, Alamat, noHP) VALUES
-('joko', 'Jl. Ciumbuleuit No.94', '081234567890');
-
--- Insert Table Bahan Baku
-INSERT INTO BahanBaku(idBahanBaku, namaBahanBaku, hargaBeliBahan, hargaJualBahan) VALUES
-('1', 'cotton20s', '', ''),
-('2', 'cotton30s', '', ''),
-('3', 'polyester', '', ''),
-('4', 'wool', '', ''),
-('5', 'denim', '', ''),
-('6', 'sutra', '', '');
-
-
--- Insert Table Aksesoris
-INSERT INTO Aksesoris(idAksesoris, namaAksesoris, hargaBeliAksesoris, hargaJualAksesoris) VALUES
-('1', 'kancing', '', ''),
-('2', 'renda', '', ''),
-('3', 'taliHoodie', '', '');
-
-
--- Insert Table Model Baju
-INSERT INTO ModelBaju(idModel, namaModel, hargaModel) VALUES
-('1', 'crewneck', ''),
-('2', 'hoodie', ''),
-('3', 'hoodieZip', ''),
-('4', 'kaosPolo', ''),
-('5', 'kaosVNeck', ''),
-('6', 'kaos', ''),
-('7', 'turtleneck', '');
-
-
--- Insert Table Baju
-INSERT INTO Baju(idBaju, Ukuran, idBahanBaku, idAksesoris, idModel, hargaBaju, hargaJahit) VALUES
-('1', 'L', '1', '1', '1', '0', '3000');
-
+INSERT INTO Pengguna(idPengguna, Email, Username, Pass, namaPengguna, peran) VALUES
+(1, 'admin@gmail.com', 'admin', 'admin', 'admin', 'admin');
 
 -- Insert Table Kurir
 INSERT INTO Kurir(idKurir, namaKurir) VALUES
-('1', 'JNE'),
-('2', 'J&T'),
-('3', 'SiCepat');
-
-
--- Insert Table Transaksi
-INSERT INTO Transaksi(idTransaksi, idPengguna, idKurir, tglTransaksi, statPemesanan, statPembayaran, buktiTransfer) VALUES
-('1', '1', '1', '20220527', 'Pembuatan Pola', 'Sudah', '');
-
-SELECT *
-FROM Pengguna;
--- 
+(1, 'JNE'),
+(2, 'J&T'),
+(3, 'SiCepat');
